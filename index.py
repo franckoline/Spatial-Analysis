a9e105c9f2b2fe320b1d06a602db773179953a37
franckoline@hotmail.com <franckoline@hotmail.com>
2020-04-20 22:39:07 +0100

    FaceOne

--- index.py
+++ index.py
@@ -10,23 +10,52 @@ import json
 import pyproj
 import os
 
+global host_name
+global url2
+
+
+#Global definitions
+def get_stuff_from_geoserver(stuff):
+    poly_search_attribute = "nuts3name"
+    poly_search_value = "south-west"
+    point_search_attribute = "population"
+    point_search_value = 5000
+
+    geo_files = {}
+    for k,v in stuff.items():
+        geo_files[k] = json.loads(rfn.get_stuff_from_net(v))
+
+    # filtered_polys = [feature for feature in geo_files["counties_url"]["features"]
+    #                   if poly_search_value in feature["properties"][poly_search_attribute].lower()]
+    filtered_polys = []
+    for feature in geo_files["counties_url"]["features"]:
+        if poly_search_value in feature["properties"][poly_search_attribute].lower():
+            filtered_polys.append(feature)
+
+    filtered_points = [point for point in geo_files["geonames_url"]["features"]
+                       if point["properties"][point_search_attribute] >= point_search_value]
+
+    merged_polys = [shape(feature["geometry"]) for feature in filtered_polys]
+    merged_poly = unary_union(merged_polys)
+
+    # points_in_merged_poly = [point for point in filtered_points
+    #                          if merged_poly.contains(shape(point["geometry"]))]
+    points_in_merged_poly = []
+    for point in filtered_points:
+        if merged_poly.contains(shape(point["geometry"])):
+            points_in_merged_poly.append(point)
+
+
+
 # Downloading geojson files
-def geoJson_Download_elements():
-    print('url element File downloaded')
-    
-def geoJson_Download_string():
-    print('string url File downloaded')
     
 def single_plot():
     print('single polygon plot')
     
-def compute_Centroid():
-    print('Computing Centroid of polygon')
-    
 def compute_convexHull():
     print('Compute convex Hull of points')
 
-def compute_centroidWithElement():
+def compute_Centroid():
     print('Compute Centroids with predefined elements')
 
 def compute_distanceBtw():
@@ -52,58 +81,97 @@ from tkinter import Scrollbar as sb
 
 class MyGUI:
     def __init__(self, my_parent):
-
-        host_name = StringVar()
-        layer_id = StringVar()
-        srs_id = int()
-        property_id = StringVar()
-        geom_id = StringVar()
-        fproperty_id = StringVar()
-        fvalue_id1 = StringVar()
-        fvalue_id2 = StringVar()
-
-        PARAMS = {
-            "host": host_name,
-            "layer": layer_id,
-            "srs_code": srs_id,
-            "properties": [property_id],
-            "geom_field": geom_id,
-            "filter_property": fproperty_id,
-            "filter_values": [fvalue_id1],
-            "filter_values": [fvalue_id2]
-        }
         self.my_parent = my_parent
 
         my_parent.title("GIS Programming - Supplement 2020 -- Name:")
 
         my_parent.protocol("WM_DELETE_WINDOW", self.catch_destroy)
 
-         
         self.frame1 = ttk.Frame(my_parent, padding=5, border=1)
         self.frame1.grid(row=0, column=0 )
+
+        # Download using urls elements
+        def geoJson_Download_elements():
+            try:
+                host = hostName.get()
+                selectLayer = layer.get()
+                geometryField = geoField.get()
+                property_id = props.get()
+                source = sourceCode.get()
+
+                self.log_text.insert(END, "Here are the inserted url element for your geoJson file: \n")
+                self.log_text.insert(END, "-" * 30 + "\n")
+                self.log_text.insert(END, "Host: " + host+"\n")
+                self.log_text.insert(END, "Layer: " + selectLayer+"\n")
+                self.log_text.insert(END, "Geometry Field: " + geometryField+"\n")
+                self.log_text.insert(END, "Property: " + property_id+"\n")
+                self.log_text.insert(END, "Source Code: " + source+"\n")
+
+
+            except Exception as e:
+                self.log_text.insert(END, "-" * 30 + "\n")
+                self.log_text.insert(END, "There is an error string can not be downloaded due to the following error(s): {}\n".format(e))
+                self.log_text.insert(END, "-" * 30 + "\n")
+                return
+
         self.tasks_frame = LabelFrame(self.frame1, padx=15, pady=15, text="Download GeoJson File with unit elements")
         self.tasks_frame.grid(row=0, column=0, sticky=NW)
-        
+
+        hostName = ttk.Entry(self.tasks_frame, width=30)
         Label(self.tasks_frame, text="Hostname: ", justify=RIGHT).grid(row=1, column=0, sticky=W)
-        Entry(self.tasks_frame, width=30, textvariable=host_name).grid(row=1, column=1, sticky=W)
+        hostName.grid(row=1, column=1, sticky=W)
+
+        layer = ttk.Entry(self.tasks_frame, width=30)
         Label(self.tasks_frame, text="Layer:", justify=RIGHT).grid(row=2, column=0, sticky=W)
-        Entry(self.tasks_frame, width=30, textvariable=layer_id).grid(row=2, column=1, sticky=W)
+        layer.grid(row=2, column=1, sticky=W)
+
+        geoField = ttk.Entry(self.tasks_frame, width=30)
         Label(self.tasks_frame, text="Geometry field: ", justify=RIGHT).grid(row=3, column=0, sticky=W)
-        Entry(self.tasks_frame, width=30, textvariable=geom_id).grid(row=3, column=1, sticky=W)
+        geoField.grid(row=3, column=1, sticky=W)
+
+        props = ttk.Entry(self.tasks_frame, width=30)
         Label(self.tasks_frame, text="Properties: ", justify=RIGHT).grid(row=1, column=3, sticky=W)
-        Entry(self.tasks_frame, width=30, textvariable=property_id).grid(row=1, column=4, sticky=W)
+        props.grid(row=1, column=4, sticky=W)
+
+        sourceCode =ttk.Entry(self.tasks_frame, width=30)
         Label(self.tasks_frame, text="Source Code: ", justify=RIGHT).grid(row=2, column=3, sticky=W)
-        Entry(self.tasks_frame, width=30, textvariable=srs_id).grid(row=2, column=4, sticky=W)
+        sourceCode.grid(row=2, column=4, sticky=W)
+
         Button(self.tasks_frame, width= 30, text="Download Geojson File >", command=geoJson_Download_elements) \
             .grid(row=3, column=4, sticky=NW, pady=5)
 
+        # Download using single url string
+        def geoJson_Download_string():
+            try:
+                urls1 = url1.get()
+                stuff = {}
+                stuff["counties_url"] = urls1
+                # stuff["counties_url"] = "http://193.1.33.31/geoserver/census2011/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=census2011%3Acounties&maxFeatures=50&outputFormat=application%2Fjson"
+                stuff["geonames_url"] = url2.get()
+                # stuff["geonames_url"] = "http://193.1.33.31/geoserver/TUDublin/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=TUDublin%3Ageonames_ie&outputFormat=application%2Fjson"
+                get_stuff_from_geoserver(stuff)
+                self.log_text.insert(END, "string url File download status link succecssful\n")
+                self.log_text.insert(END, "-" * 30 + "\n")
+
+            except Exception as e:
+                self.log_text.insert(END, "-" * 30 + "\n")
+                self.log_text.insert(END, "There is an error string can not be downloaded due to the following error(s): {}\n".format(e))
+                self.log_text.insert(END, "-" * 30 + "\n")
+                return
+
         self.tasks_frame = LabelFrame(self.frame1, padx=15, pady=15, text="Download GeoJson File using url string")
         self.tasks_frame.grid(row=3, column=0, sticky=NW)
-        Label(self.tasks_frame, text="URL: ", justify=LEFT).grid(row=7, column=0, sticky=W)
-        Entry(self.tasks_frame, width=30, textvariable=host_name).grid(row=7, column=1, sticky=W)
-        Button(self.tasks_frame, width= 30, text="Download Geojson File >", command=geoJson_Download_string) \
-            .grid(row=7, column=2, sticky=NW, pady=5)
+        url1 = ttk.Entry(self.tasks_frame, width=30)
+        Label(self.tasks_frame, text="counties URL: ", justify=LEFT).grid(row=7, column=0, sticky=W)
+        url1.grid(row=7, column=1, sticky=W)
 
+        url2 = ttk.Entry(self.tasks_frame, width=30)
+        Label(self.tasks_frame, text="geonames URL: ", justify=LEFT).grid(row=7, column=2, sticky=W)
+        url2.grid(row=7, column=3, sticky=W)
+        Button(self.tasks_frame, width= 30, text="Download Geojson File >", command=geoJson_Download_string) \
+            .grid(row=7, column=4, sticky=NW, pady=5)
+
+        # Graph Ploting Block
         self.tasks_frame = LabelFrame(self.frame1, padx=1, pady=1, text="Plot graphs")
         self.tasks_frame.grid(row=9, column=0, sticky=NW)
         Button(self.tasks_frame, width= 30, text="Single Polygon >", command=single_plot) \
@@ -113,14 +181,39 @@ class MyGUI:
         Button(self.tasks_frame, width= 30, text="Convex Hull >", command=compute_convexHull) \
             .grid(row=10, column=3, sticky=NW, pady=5)
 
+        # Centroid Computation Block
+        def compute_centroidWithElement():
+            try:
+                fName = name.get()
+                firstElement = aValue.get()
+                secondElement = bValue.get()
+                self.log_text.insert(END, "Here are the inserted filter element for Centroid: \n")
+                self.log_text.insert(END, "-" * 30 + "\n")
+                self.log_text.insert(END, "Filter Name: " + fName+"\n")
+                self.log_text.insert(END, "Filter Value 1: " + firstElement+"\n")
+                self.log_text.insert(END, "Filter Value 2: " + secondElement+"\n")
+
+            except Exception as e:
+                self.log_text.insert(END, "-" * 30 + "\n")
+                self.log_text.insert(END, "There is an error string can not be downloaded due to the following error(s): {}\n".format(e))
+                self.log_text.insert(END, "-" * 30 + "\n")
+                return
+            print('Computing Centroid of polygon')
+
         self.tasks_frame = LabelFrame(self.frame1, padx=5, pady=5, text="Plot Centroid Using Element")
         self.tasks_frame.grid(row=11, column=0, sticky=NW)
+
+        name = ttk.Entry(self.tasks_frame, width=30)
         Label(self.tasks_frame, text="Name: ", justify=RIGHT).grid(row=12, column=2, sticky=W)
-        Entry(self.tasks_frame, width=30, textvariable=fproperty_id).grid(row=12, column=3, sticky=W)
+        name.grid(row=12, column=3, sticky=W)
+        
+        aValue = ttk.Entry(self.tasks_frame, width=30)
         Label(self.tasks_frame, text="Value A:  ",).grid(row=13, column=2, sticky=W)
-        Entry(self.tasks_frame, width=30, textvariable=fvalue_id1).grid(row=13, column=3, sticky=W)
+        aValue.grid(row=13, column=3, sticky=W)
+        
+        bValue = ttk.Entry(self.tasks_frame, width=30)
         Label(self.tasks_frame, text="Value B:  ", justify=RIGHT).grid(row=14, column=2, sticky=W)
-        Entry(self.tasks_frame, width=30, textvariable=fvalue_id2).grid(row=14, column=3, sticky=W)
+        bValue.grid(row=14, column=3, sticky=W)
         Button(self.tasks_frame, width= 30, text="Centroid >", command=compute_centroidWithElement) \
             .grid(row=13, column=4, sticky=NW, pady=2)
 
@@ -148,9 +241,9 @@ class MyGUI:
     
 
 def main_gui():
-    root = Tk()
-    MyGUI(root)
-    root.mainloop()
+    my_parent= Tk()
+    MyGUI(my_parent)
+    my_parent.mainloop()
 
 
 
